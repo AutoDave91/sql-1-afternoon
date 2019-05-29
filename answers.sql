9334cf4263fc785a88492e9ef1499b73e37215c1
david <davidwnewman91@gmail.com>
2019-05-29 15:59:38 -0500

    Table invoice Completed

--- answers.sql
+++ answers.sql
@@ -114,4 +114,27 @@ WHERE reports_to =2
 5. SELECT COUNT(city) FROM employee
 WHERE city ILIKE 'LETHBRIDGE'
 
---
+--Table - invoice 
+1. SELECT COUNT(*) FROM invoice
+WHERE billing_country ILIKE 'USA'
+
+2. SELECT * FROM invoice
+ORDER BY total DESC
+LIMIT 1
+
+3. SELECT * FROM invoice
+ORDER BY total ASC
+LIMIT 1
+
+4. SELECT * FROM invoice
+WHERE total > 5
+
+5. SELECT COUNT(*) FROM invoice
+WHERE total < 5
+
+6. SELECT COUNT(*) FROM invoice
+WHERE billing_city IN ('CA', 'TX', 'AZ')
+
+7. SELECT AVG(total) FROM invoice
+
+8. SELECT SUM(total) FROM invoice
